d12ccf63ad95f7af3b9facd848f0e87dc0797ef2
Wilsonyuyu <zyu0008@gmail.com>
2023-06-13 20:07:07 -0700

    Removed local self-signed keys.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3033;
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
-app.listen(port, () => {
+// Load the SSL certificate files
+const options = {
+  key: fs.readFileSync('private.key'),
+  cert: fs.readFileSync('certificate.crt')
+};
+
+// Create an HTTPS server with the SSL certificate
+const server = https.createServer(options, app);
+
+// Start the server
+server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
