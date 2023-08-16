68eaa82c52951d8cc387751a354e1a5cf3547297
Wilsonyuyu <zyu0008@gmail.com>
2023-08-16 13:19:02 -0700

    Enabled health check for ELB

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = 80;
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
+// Health check route
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 // Create an HTTP server
 const server = app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
